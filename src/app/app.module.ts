d6cd0b92c81dd86d4e7a48e67055691f4579a85f
Prasanth Srinivasan <prashanth.srini18@gmail.com>
2019-03-13 20:45:09 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,29 +5,37 @@ import { HeaderComponent } from './header/header.component';
 import { from } from 'rxjs';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { HomecomponentComponent } from './homecomponent/homecomponent.component';
 
-const routes: Routes = [
-  { path: '', component: AuthComponent }
-  // { path: 'path', component: FeatureComponent },
-  // { path: '**', component: PageNotFoundComponent },
+const approutes: Routes = [
+  { path: 'Home', component: HomecomponentComponent },
+  { path: 'Signin', component: SigninComponent },
+  { path: 'Signup', component: SignupComponent }
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  declarations: [AuthComponent]
-})
-export class AppRoutingModule {}
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule],
+//   declarations: [AuthComponent, SignupComponent, SigninComponent, HomecomponentComponent]
+// })
+// export class AppRoutingModule {}
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomecomponentComponent,
+    SigninComponent,
+    SignupComponent,
+    AuthComponent
   ],
+  // exports: [RouterModule],
   imports: [
     BrowserModule,
-    RouterModule
+    RouterModule.forRoot(approutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
